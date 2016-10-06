4207610c27b6469a7be510d4c9dab664195657a2
andysmith <andys_81@hotmail.com>
2016-10-06 11:48:35 +0800

    started job views and routes

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,11 @@
 class Job < ActiveRecord::Base
 	belongs_to :user
+
+	# validates :avatars, presence: true
+	
+	# has_many :skills, through: :job_skills
+	# has_many :reservations, :dependent => :destroy
+
+	# mount_uploaders :avatars, AvatarUploader
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
+
+  root 'jobs#index'
+ 
+end
+
+
+
+
+ # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
@@ -53,4 +61,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
