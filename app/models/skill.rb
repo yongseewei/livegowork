a66f1dbd4f8cd6546b924bc834d23a5b624aab31
andysmith <andys_81@hotmail.com>
2016-10-11 12:18:30 +0800

    User profile page and edit profile page working, except for skills

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,4 +1,4 @@
 class Skill < ActiveRecord::Base
-	has_many :users, through: :user_skills 
-	has_many :user_skills
+	# has_many :users, through: :user_skills 
+	has_many :user_skills, :dependent => :destroy
 end
