dfe95e2678cfc802644e00d94c5c1eb19002c2f9
andysmith <andys_81@hotmail.com>
2016-10-11 14:55:42 +0800

    User can now upload photos and add skills.

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,12 +17,9 @@ class User < ActiveRecord::Base
   has_many :skills, through: :user_skills
   has_many :user_skills
 
-  # ratyrate_rateable 'score'
-  # ratyrate_rater
-
   has_many :authentications, :dependent => :destroy
 
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" ,small_thumb: "50x50>"}, default_url: "/images/:style/missing.png"
+  has_attached_file :avatar, styles: { medium: "140x140#", thumb: "100x100#" ,small_thumb: "50x50#"}, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
 
